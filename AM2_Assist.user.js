7cb3d93d32717e7f260de207a7f96177e305c334
statm <arcus.zhao@gmail.com>
2018-07-16 14:41:24 -0700

    Fix comments

--- AM2_Assist.user.js
+++ AM2_Assist.user.js
@@ -282,11 +282,12 @@
         $("#box2").after(reconfigBox);
     }, "RECONFIGURATION ASSIST");
 
-    /* MAXIMIZE LOAN AMOUNT */
+    /* MAXIMIZE LOAN AMOUNT (FM) */
     define(["finances/bank/[0-9]+/stockMarket/request"], function() {
         $("#request_amount").val($("#request_amount").attr("data-amount"));
     }, "MAXIMIZE LOAN AMOUNT (FM)");
 
+    /* MAXIMIZE LOAN AMOUNT (EXPRESS) */
     define(["finances/bank/loan/[0-9]+/express"], function() {
         $("#form_amount").val(getIntFromElement($("#loanExplanation div:nth-child(1) span")));
     }, "MAXIMIZE LOAN AMOUNT (EXPRESS)");
