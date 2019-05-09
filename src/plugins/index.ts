45cdb8b4f2cb62d1c7ffd36d1c9b2e216274b753
R Zhao <arcus.zhao@gmail.com>
2019-05-08 17:37:21 -0700

    Revamped workshop scanner

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -12,16 +12,13 @@ import { pricePerSeat } from './pricePerSeat';
 import { aircraftFiltering } from './aircraftFiltering';
 import { aircraftPurchaseListStickyHeader } from './aircraftPurchaseListStickyHeader';
 import { duperSim } from './duperSim';
-import { tcRateDisplay } from './tcRateDisplay';
 import { routeFiltering } from './routeFiltering';
 import { routePurchaseListStickyHeader } from './routePurchaseListStickyHeader';
 import { auditPriceApplying } from './auditPriceApplying';
 import { auditListEnhancement } from './auditListEnhancement';
-import { playerDataCollection } from './playerDataCollection';
-import { aircraftInfoCollection } from './aircraftInfoCollection';
-import { workshopScan } from './workshopScan';
 import { aircraftListProDisplay } from './aircraftListProDisplay';
 import { routeListProDisplay } from './routeListProDisplay';
+import { workshopScan } from './workshopScan';
 
 export function registerAllPlugins() {
     registerPlugin(slotMachineAutomation);
@@ -36,15 +33,15 @@ export function registerAllPlugins() {
     registerPlugin(aircraftFiltering);
     registerPlugin(aircraftPurchaseListStickyHeader);
     registerPlugin(duperSim);
-    registerPlugin(tcRateDisplay);
     registerPlugin(routeFiltering);
     registerPlugin(routePurchaseListStickyHeader);
     registerPlugin(auditPriceApplying);
     registerPlugin(auditListEnhancement);
-    registerPlugin(workshopScan);
     registerPlugin(aircraftListProDisplay);
     registerPlugin(routeListProDisplay);
+    registerPlugin(workshopScan);
 
-    registerPlugin(playerDataCollection);
-    registerPlugin(aircraftInfoCollection);
+    // dev purpose only
+    // registerPlugin(playerDataCollection);
+    // registerPlugin(aircraftInfoCollection);
 }
