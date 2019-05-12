3925abb0e4a0923e9e72c3833031d48e7cf8bd20
Jiakuan Li <jiakuan.li@yahoo.com>
2019-05-12 08:14:08 +0800

    one click collect all finishd item
    Signed-off-by: Jiakuan Li <jiakuan.li@yahoo.com>

---
+++ src/plugins/collectDeliveries.ts
@@ -0,0 +1,42 @@
+import { Plugin } from '../plugin';
+
+function addButton() {
+    const button = $(`
+        <li class="collectButton amCoinsPurchase">
+            <div>
+                <a id="collectButton" class="validBtnBlue">
+                    <div>
+                        <p>Deliver All</p>
+                    </div>
+                </a>
+            </div>
+        </li>
+    `);
+    button.insertAfter('li.amCoinsPurchase');
+    $('li.deliverAll').remove();
+}
+
+async function collect() {
+    const b = $('div.date a');
+    for (let i = 0; i < b.length; i++) {
+        if (!b[i].className.includes('hidden')) {
+            const link = b[i].getAttribute('href');
+            // console.log(b[i].getAttribute('href'));
+            try {
+                await $.get(`${link}`);
+                $('#rightInfoBoxContent li')[i].remove();
+            } catch (e) {
+                console.log('error on getting ' + link);
+            }
+        }
+    }
+}
+
+export const collectDeliveries: Plugin = {
+    name: 'Collect Deliveries',
+    urlPatterns: ['.*'],
+    action: function() {
+        addButton();
+        $('li.collectButton').click(collect);
+    }
+};
